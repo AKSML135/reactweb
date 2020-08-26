64c42620f1bbe382736d36e92ee0b9bf9de4dfcd
Savior135 <srivastava.amankumar03@gmail.com>
2020-08-26 21:46:49 +0530

    Updates

---
+++ precache-manifest.c9e4f460078d7ff78584b32f6d499240.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52003eb0362f958073c612c1d30f80db",
+    "url": "/reactweb/index.html"
+  },
+  {
+    "revision": "de2622ec6a1aad733734",
+    "url": "/reactweb/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "af30add493b25460d380",
+    "url": "/reactweb/static/css/main.299e6d79.chunk.css"
+  },
+  {
+    "revision": "de2622ec6a1aad733734",
+    "url": "/reactweb/static/js/2.34a5187a.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/reactweb/static/js/2.34a5187a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af30add493b25460d380",
+    "url": "/reactweb/static/js/main.77909206.chunk.js"
+  },
+  {
+    "revision": "8aae89efd13c8a974f78",
+    "url": "/reactweb/static/js/runtime-main.ce893b3e.js"
+  },
+  {
+    "revision": "6ace28239e71973e0879363ab367e52d",
+    "url": "/reactweb/static/media/477399-PGX1E8-696.6ace2823.jpg"
+  },
+  {
+    "revision": "79d352a83c5615de30c84fb3378c3321",
+    "url": "/reactweb/static/media/6123.79d352a8.jpg"
+  },
+  {
+    "revision": "60d5b5bbdee70133a31af885e92af704",
+    "url": "/reactweb/static/media/android.60d5b5bb.jpg"
+  },
+  {
+    "revision": "bde02d27fabb62d9c764fb547040972e",
+    "url": "/reactweb/static/media/app1.bde02d27.jpg"
+  },
+  {
+    "revision": "8900f75fe0e7733cccbb5526c68cbd78",
+    "url": "/reactweb/static/media/dig1.8900f75f.jpg"
+  },
+  {
+    "revision": "4e11b5420868c143aff13d61fe927e0b",
+    "url": "/reactweb/static/media/market.4e11b542.png"
+  },
+  {
+    "revision": "b6c86899c88aeda3f524cc7b0d9f4ee8",
+    "url": "/reactweb/static/media/software.b6c86899.jpeg"
+  },
+  {
+    "revision": "57e269d772efb557362226367e9e2a63",
+    "url": "/reactweb/static/media/web1.57e269d7.jpeg"
+  },
+  {
+    "revision": "961d3b3aa909679bea8ce461967a52da",
+    "url": "/reactweb/static/media/webdev1.961d3b3a.png"
+  },
+  {
+    "revision": "ca1ea804473ff7c647d772fecc2055a3",
+    "url": "/reactweb/static/media/webdev2.ca1ea804.jpg"
+  }
+]);
